0dc469ae7ab8b5a1d276c127d7f2d48c6bf5d02c
Victor Costa <victor.efetivos@gmail.com>
2018-04-15 12:37:43 -0300

    Menu Mob | Done

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
+Tempo: 11:00h
+Data: 00hrs | 12.05
+
 ## Wordpress Installation | MAMP
 > Menu MAMP > Preferences > Set Web & MySQL port to 80  & 3306 (!!! IMPORTANT)
 >entrada no BROWSER: /http://localhost/
+
+
+
 
 - cli run: gulp changed
 ```bash
--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
-TweenLite.to('p', 2,{x:400, repeat: -1, yoyo:true})
-TweenLite.to('h1', 2,{x:400, repeat: -1, yoyo:true}) 
+var menuOpen = false;
+var tlMenuMob = new TimelineMax({ paused: true })
+    .from('.menu-mob', 1.2, { width: 0, ease: Power3.easeInOut, right: 0 })
+    .staggerFrom('.box-logo-menu, .box-links-mob a', 1.2, { autoAlpha: 0, y: 80, ease: Power2.easeOut }, 0.1, '-=.2')
+    .addPause().add('exitMenu')
+    .to('.menu-mob', 1.6, { width: 0, ease: Power3.easeInOut, left: 0 })
+    .to('.menu-mob a, .box-logo-menu, .desenvolvido', 1, { autoAlpha: 0, x: -50, ease: Power3.easeInOut }, '-=1.4')
+    .add(function () { menuOpen = false })
+    .to('.menu-mob', .00001, { width: 0, })    
+    .add(function () { $('.line').text('contato'); },'-=.6');
+
+$('.holder-trg-menu, .menu-mob .box-links-mob a, .menu-mob').click(function () {
+    if (!menuOpen) {
+        menuOpen = true;
+        tlMenuMob.play(0);
+        $('.line').text('fechar');
+    } else {
+        tlMenuMob.play('exitMenu');
+    }
+})
+
+$(".ctn-trigger-menu").click(function () {
+
+    tlTriggerMenu.reversed(!tlTriggerMenu.reversed());
+});
